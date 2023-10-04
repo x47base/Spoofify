f387053c9481683f2718e74da855477da4db1616
KSH <spinks@bzz.ch>
2023-10-04 13:13:50 +0200

    Adding Modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,6 +1,6 @@
 export default function Modal({ title, description, button_text }) {
     return (
-        <div className="modal h-screen flex justify-center items-center w-screen z-50">
+        <div className="modal h-screen flex justify-center items-center w-screen">
             <div className="rounded-md bg-neutral-800 border-none border-r-2 p-4 flex flex-col">
                 <h3 className="font-semibold text-white text-2xl ">{title}</h3>
                 <span className="text-white font-normal">{description}</span>
