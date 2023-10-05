cf6abc5f7f50441c053a81a35fb07fe253e11491
KSH <spinks@bzz.ch>
2023-10-05 12:55:13 +0200

    Update Formatting & Structure

--- src/App.js
+++ src/App.js
@@ -12,22 +12,57 @@ import {
 import { TiImageOutline } from "react-icons/ti";
 import "./App.css";
 
-import Form from "./Form";
-import Modal from "./Modal";
+import Form from "./components/Form";
+import Modal from "./components/Modal";
 
 /* schema: { title: "", artist: "", duration: 0, image_url: "", path: ""} */
 let sounds = [
-  { title: "Invincible", artist: "DEAF KEV", duration: 213, image_url: "https://i1.sndcdn.com/artworks-000224592805-nhfd2x-t500x500.jpg", path: "./sounds/DEAF KEV - Invincible [NCS Release] [J2X5mJ3HDYE].mp3"},
-  { title: "Invisible", artist: "Julius Dreisig & Zeus feat. Crona", duration: 201, image_url: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/548/1000x0/invisible-1586956245-t2ybbYq7Q8.jpg", path: "./sounds/Julius Dreisig & Zeus X Crona - Invisible [NCS Release] [QglaLzo_aPk].mp3"},
-	{ title: "Fight Back", artist: "NEFFEX", duration: 196, image_url: "https://i.scdn.co/image/ab67616d0000b273bd9e9490d5198c41cb85b669", path: "./sounds/Neffex - Fight Back.mp3" },
-  { title: "Best of Me", artist: "NEFFEX", duration: 239, image_url: "https://i1.sndcdn.com/artworks-000233271395-45pahr-t500x500.jpg", path: "./sounds/NEFFEX - Best of Me.mp3"},
-  { title: "Never Give Up", artist: "NEFFEX", duration: 251, image_url: "https://i1.sndcdn.com/artworks-000237594251-ibzt3b-t500x500.jpg", path: "./sounds/NEFFEX - Never Give Up.mp3"}
-]
+  {
+    title: "Invincible",
+    artist: "DEAF KEV",
+    duration: 213,
+    image_url:
+      "https://i1.sndcdn.com/artworks-000224592805-nhfd2x-t500x500.jpg",
+    path: "./sounds/DEAF KEV - Invincible [NCS Release] [J2X5mJ3HDYE].mp3",
+  },
+  {
+    title: "Invisible",
+    artist: "Julius Dreisig & Zeus feat. Crona",
+    duration: 201,
+    image_url:
+      "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/548/1000x0/invisible-1586956245-t2ybbYq7Q8.jpg",
+    path: "./sounds/Julius Dreisig & Zeus X Crona - Invisible [NCS Release] [QglaLzo_aPk].mp3",
+  },
+  {
+    title: "Fight Back",
+    artist: "NEFFEX",
+    duration: 196,
+    image_url:
+      "https://i.scdn.co/image/ab67616d0000b273bd9e9490d5198c41cb85b669",
+    path: "./sounds/Neffex - Fight Back.mp3",
+  },
+  {
+    title: "Best of Me",
+    artist: "NEFFEX",
+    duration: 239,
+    image_url:
+      "https://i1.sndcdn.com/artworks-000233271395-45pahr-t500x500.jpg",
+    path: "./sounds/NEFFEX - Best of Me.mp3",
+  },
+  {
+    title: "Never Give Up",
+    artist: "NEFFEX",
+    duration: 251,
+    image_url:
+      "https://i1.sndcdn.com/artworks-000237594251-ibzt3b-t500x500.jpg",
+    path: "./sounds/NEFFEX - Never Give Up.mp3",
+  },
+];
 var playing = -1;
 
-function sound_length_to_text(duration){
-  let a = (duration / 60).toFixed(2).toString().replace(".",":")
-  return a
+function sound_length_to_text(duration) {
+  let a = (duration / 60).toFixed(2).toString().replace(".", ":");
+  return a;
 }
 
 let playingInterval;
@@ -42,48 +77,52 @@ function stopPlaying() {
 
 function updateSlider() {
   let soundElement = sounds[playing];
-  let base = `${soundElement.title.replace(/\s/g, '')}-${soundElement.artist}`;
-	let audioFile = document.getElementById(`sound-${base}`);
-  let current_time = document.getElementById('songCurrentTime')
-  let current_max_time = document.getElementById('songLength')
-  let current_time_slider = document.getElementById('songCurrentTimeSlider')
+  let base = `${soundElement.title.replace(/\s/g, "")}-${soundElement.artist}`;
+  let audioFile = document.getElementById(`sound-${base}`);
+  let current_time = document.getElementById("songCurrentTime");
+  let current_max_time = document.getElementById("songLength");
+  let current_time_slider = document.getElementById("songCurrentTimeSlider");
   let duration = audioFile.duration;
 
   if (!isNaN(duration)) {
     const currentTime = (audioFile.currentTime / duration) * 100;
     current_time_slider.value = currentTime;
-    current_time.innerText = sound_length_to_text(audioFile.currentTime)
-    current_max_time.innerText = sound_length_to_text(duration)
+    current_time.innerText = sound_length_to_text(audioFile.currentTime);
+    current_max_time.innerText = sound_length_to_text(duration);
   }
 }
 
-function setup_sound(soundElement){
-	let title = soundElement.title;
-	let artist = soundElement.artist;
-	let duration = soundElement.duration;
-	let path = soundElement.path;
+function setup_sound(soundElement) {
+  let title = soundElement.title;
+  let artist = soundElement.artist;
+  let duration = soundElement.duration;
+  let path = soundElement.path;
 
-  let current_title = document.getElementById('songCurrentlyPlaying')
-  let current_time = document.getElementById('songCurrentTime')
-  let current_time_slider = document.getElementById('songCurrentTimeSlider')
-  let current_max_time = document.getElementById('songLength')
+  let current_title = document.getElementById("songCurrentlyPlaying");
+  let current_time = document.getElementById("songCurrentTime");
+  let current_time_slider = document.getElementById("songCurrentTimeSlider");
+  let current_max_time = document.getElementById("songLength");
 
-  if(path == undefined){
-    path = `${artist} - ${title}.mp3`
+  if (path == undefined) {
+    path = `${artist} - ${title}.mp3`;
   }
 
-	let base = `${title.replace(/\s/g, '')}-${artist}`;
-	let btn = document.getElementById(`btn-${base}`);
-	let audioFile = document.getElementById(`sound-${base}`);
+  let base = `${title.replace(/\s/g, "")}-${artist}`;
+  let btn = document.getElementById(`btn-${base}`);
+  let audioFile = document.getElementById(`sound-${base}`);
 
-  let playIcon = '<div class="sidebar-icon"><div class="text-white hover:text-green-500"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"></path></svg></div></div>'
-  let pauseIcon = '<div class="sidebar-icon"><div><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"></path></svg></div></div>'
+  let playIcon =
+    '<div class="sidebar-icon"><div class="text-white hover:text-green-500"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"></path></svg></div></div>';
+  let pauseIcon =
+    '<div class="sidebar-icon"><div><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"></path></svg></div></div>';
 
-	btn.onclick = function(){
-		if (audioFile.paused == true){
-      if(playing != -1){
-        let soundElement2 = sounds[playing]
-        let base2 = `${soundElement2.title.replace(/\s/g, '')}-${soundElement2.artist}`;
+  btn.onclick = function () {
+    if (audioFile.paused == true) {
+      if (playing != -1) {
+        let soundElement2 = sounds[playing];
+        let base2 = `${soundElement2.title.replace(/\s/g, "")}-${
+          soundElement2.artist
+        }`;
         let btn2 = document.getElementById(`btn-${base2}`);
         let audioFile2 = document.getElementById(`sound-${base2}`);
 
@@ -93,58 +132,59 @@ function setup_sound(soundElement){
 
       audioFile.src = path;
       audioFile.play();
-      startPlaying()
+      startPlaying();
       btn.children[0].innerHTML = pauseIcon;
 
       current_title.innerText = `${title} - ${artist}`;
       current_time.innerText = audioFile.currentTime;
-      
+
       current_max_time.innerText = sound_length_to_text(audioFile.duration);
 
-      playing = sounds.indexOf(soundElement)
-		} else {
-			audioFile.pause();
-      stopPlaying()
+      playing = sounds.indexOf(soundElement);
+    } else {
+      audioFile.pause();
+      stopPlaying();
       btn.children[0].innerHTML = playIcon;
-			current_title.innerText = 'Untitled';
-			current_time.innerText = "0:00";
-			
-			current_max_time.innerText = "0:00";
-		}
-	}
+      current_title.innerText = "Untitled";
+      current_time.innerText = "0:00";
 
-/*
+      current_max_time.innerText = "0:00";
+    }
+  };
+
+  /*
 	while(audioFile.paused != true){
 		current_time_slider.value = audioFile.currentTime;
 	}
 */
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-  for(let sound of sounds){
-    setup_sound(sound)
+document.addEventListener("DOMContentLoaded", function () {
+  for (let sound of sounds) {
+    setup_sound(sound);
   }
-  
-  let current_time_slider = document.getElementById('songCurrentTimeSlider')
-  current_time_slider.addEventListener('input', () => {
-    if(playing != -1){
-      let soundElement = sounds[playing]
-      let base = `${soundElement.title.replace(/\s/g, '')}-${soundElement.artist}`;
+
+  let current_time_slider = document.getElementById("songCurrentTimeSlider");
+  current_time_slider.addEventListener("input", () => {
+    if (playing != -1) {
+      let soundElement = sounds[playing];
+      let base = `${soundElement.title.replace(/\s/g, "")}-${
+        soundElement.artist
+      }`;
       let btn = document.getElementById(`btn-${base}`);
       let audioFile = document.getElementById(`sound-${base}`);
       let percentage = current_time_slider.value;
       let duration = audioFile.duration;
-  
-      if (!isNaN(duration)) {
-          const currentTime = (percentage / 100) * duration;
-          audioFile.currentTime = currentTime;
-      }
 
+      if (!isNaN(duration)) {
+        const currentTime = (percentage / 100) * duration;
+        audioFile.currentTime = currentTime;
+      }
     }
-  })
+  });
 
   // ...
-})
+});
 
 const SideBarIcon = ({ icon, classes }) => {
   return (
@@ -163,16 +203,25 @@ const SideBar = () => {
       <div className="flex flex-col gap-4 justify-between">
         <div className="upper-container flex flex-col justify-between gap-4 sidebar-main">
           <a href="/" className="sidebar__link">
-            <SideBarIcon icon={<BsFillHouseFill size="28" />} classes="hover:text-green-500 active-tab" />
+            <SideBarIcon
+              icon={<BsFillHouseFill size="28" />}
+              classes="hover:text-green-500 active-tab"
+            />
           </a>
 
           <a href="#" className="sidebar__link">
-            <SideBarIcon icon={<BsFillBookmarksFill size="28" />} classes="hover:text-green-500" />
+            <SideBarIcon
+              icon={<BsFillBookmarksFill size="28" />}
+              classes="hover:text-green-500"
+            />
           </a>
         </div>
         <div className="lower-container sidebar-other fixed bottom-0 left-0 w-32 mb-6">
           <a href="#" className="sidebar__link">
-            <SideBarIcon icon={<BsFillGearFill size="28" />} classes="hover:text-green-500" />
+            <SideBarIcon
+              icon={<BsFillGearFill size="28" />}
+              classes="hover:text-green-500"
+            />
           </a>
         </div>
       </div>
@@ -193,7 +242,9 @@ const Player = () => {
         <span
           id="songCurrentTime"
           className="text-textLight text-base font-medium"
-        >0:00</span>
+        >
+          0:00
+        </span>
         <input
           id="songCurrentTimeSlider"
           type="range"
@@ -202,10 +253,9 @@ const Player = () => {
           className="cursor-pointer"
           onChange={() => {}}
         />
-        <span
-          id="songLength"
-          className="text-textLight text-base font-medium"
-        >0:00</span>
+        <span id="songLength" className="text-textLight text-base font-medium">
+          0:00
+        </span>
         <button nameClass="btn">
           <SideBarIcon icon={<BsFillHandThumbsUpFill size="18" />} />
         </button>
@@ -246,7 +296,10 @@ const Track = ({ image_url, title, artist, length, sound_url }) => {
     <div nameClass="track flex flex-row mx-auto rounded-sm transition-colors hover:bg-hover cursor-pointer hover:text-hover">
       <div className="flex flex-row justify-between p-2 items-center">
         <div className="flex col-span-1 justify-evenly">
-          <button nameClass="btn" id={`btn-${title.replace(/\s/g, '')}-${artist}`}>
+          <button
+            nameClass="btn"
+            id={`btn-${title.replace(/\s/g, "")}-${artist}`}
+          >
             <SideBarIcon
               icon={<BsPlayFill size="20" />}
               classes="text-white hover:text-green-500"
@@ -261,7 +314,10 @@ const Track = ({ image_url, title, artist, length, sound_url }) => {
         <span className="text-white">{title}</span>
         <span className="text-white">{artist}</span>
         <span className="text-white">{length}</span>
-        <audio id={`sound-${title.replace(/\s/g, '')}-${artist}`} src={sound_url}/>
+        <audio
+          id={`sound-${title.replace(/\s/g, "")}-${artist}`}
+          src={sound_url}
+        />
       </div>
     </div>
   );
@@ -298,23 +354,27 @@ function Footer() {
 }
 */
 
+/* Example Register Form
+
+      <Form title="Create Account" submit_text="Register"
+        inputs = {[
+          { action: "/", method: "POST", type: "email", name: "p-email", placeholder: "example@gmail.com", minLength: "" },
+          { action: "/", method: "POST", type: "password", name: "p-password", placeholder: "Password", minLength: "8" },
+        ]}/>
+*/
+
 function App() {
   return (
     <div>
-      <Modal title="Welcome" description="Welcome to Spoofify! The #1 Music Platform."/>
-      <Form title="Create Account" submit_text="Register"
-        inputs = {[
-          { type: "text", name: "p-fname", placeholder: "Vorname" },
-          { type: "email", name: "p-email", placeholder: "example@gmail.com" },
-          { type: "number", name: "p-age", placeholder: "Alter" },
-
-        ]}/>
+      <Modal
+        title="Welcome"
+        description="Welcome to Spoofify! The #1 Music Platform."
+      />
 
       <div className="App">
         <SideBar />
         <Player />
         <Main />
-
       </div>
     </div>
   );
