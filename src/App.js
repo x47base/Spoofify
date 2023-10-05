da62b316699106175707cd8e4ef807afec37164f
KSH <spinks@bzz.ch>
2023-10-05 21:32:19 +0200

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -193,7 +193,7 @@ function setup_sound(soundElement) {
 function play_next_song() {
   let current_time_slider = document.getElementById("songCurrentTimeSlider");
   let player_btn = document.getElementById('player-play-btn');
-
+  
   let playIcon =
     '<div class="sidebar-icon"><div class="text-white hover:text-green-500"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"></path></svg></div></div>';
   let pauseIcon =
@@ -202,6 +202,9 @@ function play_next_song() {
   if (playing < (sounds.length - 1) && playing != -1) {
     let prebase = `${sounds[playing].title.replace(/\s/g, "")}-${sounds[playing].artist}`;
     let prebtn = document.getElementById(`btn-${prebase}`);
+    let preAudio = document.getElementById(`sound-${prebase}`);
+    preAudio.pause()
+    preAudio.currentTime = 0;
     prebtn.children[0].innerHTML = playIcon;
     playing += 1;
   } else if (playing == -1) {
